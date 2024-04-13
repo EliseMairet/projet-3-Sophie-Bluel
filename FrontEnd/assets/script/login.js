a82b784fe05a76d7441e9496a6877ceee69cd7a4
EliseMairet <elise.mairet@hotmail.com>
2024-04-13 20:02:03 +0200

    commit13/04

--- FrontEnd/assets/script/login.js
+++ FrontEnd/assets/script/login.js
@@ -1,47 +1,47 @@
 
 // varaible login
-const loginUrl = "http://localhost:5678/api/users/login";
-const inputEmail = document.getElementById("email");
-const inputPassword = document.getElementById("password");
-const submitBtn = document.querySelector("input[type='submit']");
-const form = document.getElementById("loginForm");
-const loginError = document.querySelector(".loginError");
-const passwordError = document.querySelector(".passwordError");
+const loginUrl = "http://localhost:5678/api/users/login"
+const inputEmail = document.getElementById("email")
+const inputPassword = document.getElementById("password")
+const submitBtn = document.querySelector("input[type='submit']")
+const form = document.getElementById("loginForm")
+const loginError = document.querySelector(".loginError")
+const passwordError = document.querySelector(".passwordError")
 
 const logUser = {
   email: "",
-  password: "",
-};
+  password: ""
+}
 
 
 //LOGIQUE contrôle du Log IN
 
 // Evenement à la connection
 form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  loginUser();
-});
+  e.preventDefault()
+  e.stopPropagation()
+  loginUser()
+})
 
 // Evenement au MAIL
 inputEmail.addEventListener("input", (e) => {
-  inputEmail.reportValidity();
-  logUser.email = e.target.value;
-});
+  inputEmail.reportValidity()
+  logUser.email = e.target.value
+})
 
 // Evenement au Password
 inputPassword.addEventListener("input", (e) => {
-  inputPassword.reportValidity();
-  logUser.password = e.target.value;
-});
+  inputPassword.reportValidity()
+  logUser.password = e.target.value
+})
 
 //Evenement au chargement du DOM
 document.addEventListener("DOMContentLoaded", (e) => {
-  e.preventDefault();
-  logUser.email = inputEmail.value;
-  logUser.password = inputPassword.value;
-  console.log(logUser);
-});
+  e.preventDefault()
+  logUser.email = inputEmail.value
+  logUser.password = inputPassword.value
+  console.log(logUser)
+})
 
 // faire la route users/login (methode post)
 
@@ -50,38 +50,37 @@ async function loginUser() {
     await fetch(loginUrl, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json",
+        "Content-Type": "application/json"
       },
-      body: JSON.stringify(logUser),
+      body: JSON.stringify(logUser)
     })
       .then((response) => response.json())
       .then((responseData) => {
-        data = responseData;
-        console.log(data);
-      });
+        data = responseData
+        console.log(data)
+      })
     if (data.message) {
-      loginError.textContent = "Erreur dans l’identifiant !!";
-      inputEmail.style.color = "red";
-      console.log(logUser);
+      loginError.textContent = "Erreur dans l’identifiant !!"
+      inputEmail.style.color = "red"
+      console.log(logUser)
     } else if (data.error) {
-      passwordError.textContent = "Erreur dans le mot de passe !!";
-      loginError.textContent = "";
-      inputPassword.style.color = "red";
-      inputEmail.style.color = "#1d6154";
+      passwordError.textContent = "Erreur dans le mot de passe !!"
+      loginError.textContent = ""
+      inputEmail.style.color = "#1d6154"
 
-      console.log(logUser);
+      console.log(logUser)
     } else {
-      inputPassword.style.color = "#1d6154";
-      passwordError.textContent = "";
-      loginError.textContent = "";
-      console.log("LogAdmin OK");
-      console.log(logUser);
+      inputPassword.style.color = "#1d6154"
+      passwordError.textContent = ""
+      loginError.textContent = ""
+      console.log("LogAdmin OK")
+      console.log(logUser)
       // stockage du token dans le stockage local
-      localStorage.setItem("token", data.token);
+      localStorage.setItem("token", data.token)
       //Redirection index.html
-      window.location.href = "../index.html";
+      window.location.href = "../index.html"
     }
   } catch (error) {
-    console.log(error);
+    console.log(error)
   }
 }
